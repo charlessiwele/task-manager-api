da1731c046ab552c456a3ebb5e9672e492780f51
Charles Siwele <charless@connected.co.za>
2025-02-15 22:23:21 +0200

    removed unused user manager model

--- task_manager/tm_api/models.py
+++ task_manager/tm_api/models.py
@@ -6,34 +6,6 @@ from django.contrib.auth.models import BaseUserManager
 from django.utils import timezone
 
 
-class UserManager(BaseUserManager):
-
-    use_in_migration = True
-
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Email is Required')
-        if not username:
-            raise ValueError('Username is Required')
-
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff = True')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser = True')
-
-        return self.create_user(username, email, password, **extra_fields)
-
-
 class Status(models.Model):
     name = models.CharField(
         max_length=50, 
